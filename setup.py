b048e6741334ac2b64205f728d886c9af7827209
wnm9 <haonan.yang@iqr-robot.com>
2024-11-05 10:26:24 +0800

    fix readme

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='iqr_ptu2',
     description='IQR PTU2 SDK',
+    url='https://github.com/I-Quotient-Robotics/iqr_ptu2',
     py_modules=['iqr_ptu2'],
     version='0.1',
     install_requires=[
